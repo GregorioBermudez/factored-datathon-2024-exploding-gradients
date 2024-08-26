80e3f372ff2e5e37cc0c28cfd83b9bad929644b1
Santiago López <91813052+Santilopezc@users.noreply.github.com>
2024-08-25 19:39:50 -0500

    Updated time zone awareness

--- Project_Deployment/web_app.py
+++ Project_Deployment/web_app.py
@@ -32,9 +32,17 @@ selected_category = st.sidebar.selectbox(
 )
 
 def date_selector():
+    utc_now = datetime.now(pytz.utc)
+    est_tz = pytz.timezone('US/Eastern')
+    est_now = utc_now.astimezone(est_tz)
+    news_release_time = est_now.replace(hour=6, minute=0, second=0, microsecond=0)
     yesterday = datetime.now().date() - timedelta(days=1)
     date_option = st.radio("Get news from:", ["Yesterday", "Choose dates"])
+    
     if date_option == "Yesterday":
+        if est_now < news_release_time:
+            st.warning("Yesterday's news will be released at 6:00 AM EST.")
+            return None, None
         return yesterday, yesterday
     else:
         col1, col2 = st.columns(2)
@@ -48,7 +56,7 @@ def date_selector():
             st.error("Please select a date range of 31 days or fewer.")
             return None, None
         return start_date, end_date
-
+        
 # Calculate the date range
 start_date, end_date = date_selector()
 st.write(f"Current date and hour: {datetime.now()}")
